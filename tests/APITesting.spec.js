a9aad7262907a2d32f7e90e8ba5c5ab47a44fc58
Wikan Chandra <Wikanchandrak@gmail.com>
2025-04-12 12:56:34 +0700

    test: update APITesting.spec.js

--- tests/APITesting.spec.js
+++ tests/APITesting.spec.js
@@ -1,4 +1,4 @@
-const {test, request, expect} = require ('@playwright/test')
+const {test, request} = require ('@playwright/test')
 
 test.describe('SuccessAddLaporan', ()=>{
     let APIContext;
@@ -8,7 +8,7 @@ test.describe('SuccessAddLaporan', ()=>{
             baseURL: 'https://lapor.folkatech.com/api/report',
             extraHTTPHeaders: {
               'Content-Type': 'application/json',
-              'Authorization':'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvbGFwb3IuZm9sa2F0ZWNoLmNvbVwvYXBpXC9sb2dpbiIsImlhdCI6MTc0NDM4OTU0MSwiZXhwIjoxNzQ0MzkzMTQxLCJuYmYiOjE3NDQzODk1NDEsImp0aSI6IlBmaE5yVkl2bjFVM0Z2WmciLCJzdWIiOjYsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.7gdADfo-bziOG1sranMf4s2bGtnWh2UC65y8OKGvQZM',
+              'Authorization':'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvbGFwb3IuZm9sa2F0ZWNoLmNvbVwvYXBpXC9sb2dpbiIsImlhdCI6MTc0NDQzNzIzMiwiZXhwIjoxNzQ0NDQwODMyLCJuYmYiOjE3NDQ0MzcyMzIsImp0aSI6IjZzNUR6QzRobjBtUnYxaHgiLCJzdWIiOjYsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.nqa5oao9mNwJmbzbW3yPo2QeKSwTcxIF2QrsVjdy_RQ',
         
         }
         })
@@ -20,7 +20,7 @@ test.describe('SuccessAddLaporan', ()=>{
           "id": 9107,
           "user_id": 16,
           "report_category_id": 3,
-          "description": "Ini Tes Lagi pls",
+          "description": "Testing1",
           "attachment_image": null,
           "attachment_video": null,
           "tracking_id": "TK-250318-00647",
@@ -28,7 +28,7 @@ test.describe('SuccessAddLaporan', ()=>{
           "lng": "1",
           "address_location": "Disini",
           "stat_progress": "open",
-          "created_at": "2025-04-11T15:23:41.000000Z",
+          "created_at": "2025-04-12T15:11:26.000000Z",
           "updated_at": "2025-03-18T15:05:37.000000Z",
           "fullname": "BANA",
           "province_id": 1,
@@ -42,8 +42,8 @@ test.describe('SuccessAddLaporan', ()=>{
               "name": "Jawa Barat"
           },
           "report_category": {
-              "id": 1,
-              "name": "Category A",
+              "id": 3,
+              "name": "Category C",
               "icon": "https://lapor.folkatech.com/storage/icons/20230801140826.png"
         }
       }
@@ -55,10 +55,10 @@ test.describe('SuccessAddLaporan', ()=>{
       const response = await APIContext.post('https://lapor.folkatech.com/api/report', {
         data: {
           
-      nama: 'Pengguna Tanpa Kategori',
-      provinsi: 'Jawa Barat',
-      urgensi: 'Tinggi',
-      isi: 'Laporan tanpa kategori'
+      nama: 'Nada',
+      provinsi: 'DKI Jakarta',
+      urgensi: 'rendah',
+      isi: 'Laporan Biasa'
           },
          
       })
